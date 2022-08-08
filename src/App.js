6322c0bf1f95e51c52381de4ac8d3471d6956f6d
ChereponAlex <lexa_klav@mail.ru>
2022-08-08 11:07:54 +0300

    page cluster_1/project_4

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import ItemLogisticProject from './pages/ItemLogisticProject';
 import mainpage from '../src/styles/mainpage.css';
 import cluster_1 from '../src/styles/cluster_1.css';
 import project_4 from '../src/styles/project_4.css'
-
+import React, { useState, useEffect } from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
 
 
 import '../src/fonts/Graphik-Black-Web.ttf';
@@ -15,9 +16,20 @@ import '../src/fonts/Graphik-Regular-Web.ttf';
 
 
 function App() {
+  const [loading, setloading] = useState(false);
+  useEffect(() => {
+    setloading(true)
+    setTimeout(() => {
+      setloading(false)
+    }, 8000);
+  }, []);
 
   return (
     <div className='app-container'>
+      
+      {/* {loading ? <ClipLoader  className='loader' color={'#4362ff'} loading={loading} size={150}/>
+      :  null
+      } */}
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<MainPage />} />
@@ -27,8 +39,18 @@ function App() {
         </Routes>
       </BrowserRouter>
     </div>
-    
+
   );
 }
 
 export default App;
+
+
+{/* <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/cluster/:id' element={<ItemCluster />} />
+          <Route path='/cluster/:id/project/:id' element={<ItemLogisticProject />} />
+
+        </Routes>
+      </BrowserRouter> */} 
--- src/pages/ItemLogisticProject.jsx
+++ src/pages/ItemLogisticProject.jsx
@@ -3,6 +3,8 @@ import { useParams, useNavigate } from 'react-router-dom';
 import logistic_project_4_page from '../images/logistic_project_4_page.svg';
 import { ReactComponent as Back_btn } from '../images/backButton.svg';
 import { ReactComponent as Back_home } from '../images/back_home.svg';
+import background from '../images/logistic_project_4_page.svg'
+// import button_back from '../images/button_back.jpg'
 
 
 
@@ -24,31 +26,45 @@ const ItemLogisticProject = () => {
             return (
 
                 <div className='logistic_project_4_page'>
-                    {/* <header className='header_logistic_project_4'>
-                        <div className='header_btn_back' onClick={() => navigate('/')}>
-                            <Back_btn className='123'/>
-                            <div className='header_btn_back_description'>Назад к кластеру</div>
-                            <Back_home style={{ paddingLeft: '0px' }} />
-                            <div className='header_btn_back_description'>На главную</div>
+
+                    <header className='header_logistic_project_4'>
+                        <div className='header_btn_back_container' >
+                            <div className='header_btn_back_cluster' onClick={() => navigate('/cluster/1')}>
+                                <Back_btn className='header_icons'/>
+                                <div className='header_btn_back_description'>Назад к кластеру</div>
+                            </div>
+                            <div className='header_btn_back_cluster' onClick={() => navigate('/')} style={{marginLeft: '6.04vw'}}  >
+                                <Back_home className='header_icons'/>
+                                <div className='header_btn_back_description'>На главную</div>
+                            </div>
 
                         </div>
-                    </header> */}
+                    </header>
                     <main className='main_logistic_project_4'>
-
-                        {/* <section className='section_left_logistic_project_4'>
-                            <div style={{color: 'white'}}>
+                        <section className='section_left_logistic_project_4'>
+                            <div className='description_page_logistic_project_4'>
                                 Общественно-деловой центр
                                 в городе Артем
                             </div>
-                            <div style={{color: 'white'}}>шаблон карточки проекта. на фон ставим картинку из круга. делаем заголовок, под ним описание. справа делаем место под стоимость, параметры это драфтово так давай соберем такую карточку и тогда правда утвердим все 3 уровня по шаблонам, потом будем тогда
-
+                            <div className='_description_page_logistic_project_4'>
+                                шаблон карточки проекта. на фон ставим картинку из круга. делаем заголовок, под ним описание. справа делаем место под стоимость, параметры это драфтово так давай соберем такую карточку и тогда правда утвердим все 3 уровня по шаблонам, потом будем тогда
                             </div>
-                        </section> */}
-
+                        </section>
                         <section className='section_right_logistic_project_4'>
-                            <div></div>
-                            <div></div>
-                            <div></div>
+                            <div className='block_1_right_section'>
+                                <div className='descriprion_card_project'>1,6 млрд руб.</div>
+                                <div className='card_project'>шаблон карточки проекта. на фон ставим картинку</div>
+                            </div>
+
+                            <div className='block_1_right_section'>
+                                <div className='descriprion_card_project'>12 тыс. м2</div>
+                                <div className='card_project'>шаблон карточки проекта. на фон ставим картинку</div>
+                            </div>
+
+                            <div className='block_1_right_section'>
+                                <div className='descriprion_card_project'>1,6 млрд руб.</div>
+                                <div className='card_project'>шаблон карточки проекта. на фон ставим картинку</div>
+                            </div>
 
                         </section>
 
