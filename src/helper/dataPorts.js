bf4f185e107ade5a8e453a4a817242cc373a2a0a
ChereponAlex <lexa_klav@mail.ru>
2022-08-29 14:49:10 +0300

    fix main page

---
+++ src/helper/dataPorts.js
@@ -0,0 +1,6 @@
+const dataPorts = [
+    {id: 1, title: 'Порт Судохол'},
+    {id: 2, title: 'Порт Вера'},
+]
+
+export default dataPorts;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,62 +4,21 @@ import { Link } from 'react-router-dom';
 import encodeURI from '../utils/encodeURI';
 import MapBoxContainer from '../components/MapBoxContainer';
 import { ReactComponent as Background } from '../images/background_mainpage.svg';
-import { ReactComponent as SignRusskiy } from '../images/signRusskiy.svg'
-import { ReactComponent as BigStone } from '../images/signBigStone.svg'
-import { ReactComponent as SignArtem } from '../images/signArtem.svg'
-import { ReactComponent as SignAeroport } from '../images/signAeroport.svg'
-import { ReactComponent as IconAeroport } from '../images/icon_aeroport.svg'
-import { ReactComponent as SignVladivostok } from '../images/signVladivostok.svg'
-import { ReactComponent as SignTor } from '../images/signTOR.svg'
-import { ReactComponent as SignSanding } from '../images/sign_sanding.svg'
-import { ReactComponent as Sign_bridge_elena } from '../images/sign_bridge_elena.svg'
-import { ReactComponent as Sign_island_popova } from '../images/sign_island_popova.svg'
-import { ReactComponent as Sign_island_reineke } from '../images/sign_island_reineke.svg'
-import { ReactComponent as Sea_ways } from '../images/sea_ways.svg'
-import { ReactComponent as Land_ways } from '../images/land_ways.svg'
 
-// import { ReactComponent as Background } from '../images/background_mainpage_finish.svg';
 import dataClusters from '../helper/dataClusters';
 import dataTransport from '../helper/dataTransport';
+import dataPorts from '../helper/dataPorts';
+import dataInvest from '../helper/dataInvest';
 
 
 const MainPage = () => {
-
-  // функция под навигацию кластеров
-  // const SvgContainer = (id) => {
-
-  //   const parseID = id[id.length - 1]
-  //   console.log(parseID)
-
-  //   const navigate = useNavigate();
-
-  //   useEffect(() => {
-  //     const changeRoute = (routeName) => {
-  //       navigate(`/cluster/${routeName}`)
-  //       // console.log('changeRoute', routeName)
-  //     }
-  //     console.log(parseID)
-  //     const path = document.getElementById(`path_${id}`)
-  //     !!path && path.addEventListener('click', () => {
-  //       changeRoute(id)
-  //     });
-
-  //     return () => {
-  //       !!path && path.removeEventListener('click', changeRoute)
-  //     }
-
-  //   }, []);
-  // }
 
   const navigate = useNavigate();
   const changeRoute = (id) => {
     console.log(id)
     const parseID = id[id.length - 1]
     if (parseID) navigate(`/cluster/${parseID}`)
-
   }
-
-
   return (
     <div className='main-page-background'>
       <div className='description_page'>
@@ -75,13 +34,35 @@ const MainPage = () => {
         {/* <div className='text_city artem_position'>г. Артем</div> */}
         <div className='text_city vladivostok_position'>г. Владивосток</div>
         <div className='text_city island_russian_position'>о. Русский</div>
-
-        <div className='text_city aeroport'>Аэропорт (Кневичи) <br /> имени В.К. Арсеньева</div>
+        <div className='text_city aeroport'>Международный аэропорт <br /> Владивосток им. В.К. Арсеньева</div>
         <div className='text_city tor_nadeschdin'>ТОР Надеждинская</div>
         <div className='text_city sanding'>мыс Песчаный</div>
         <div className='text_city bridge_elena'>мост на о. Елены</div>
         <div className='text_city island_popova'>о. Попова</div>
         <div className='text_city island_reineke'>о. Рейнеке</div>
+        <div className='text_city island_naumova'>о. Наумова</div>
+        <div className='text_city town_sputnik'>Город-спутник <br /> Владивостока</div>
+
+        <div>
+          {dataInvest.map(({ id, title }) => {
+            return (
+              <p key={id} className={`invest_text invest_location_${id}`}>
+                {title}
+              </p>
+            )
+          })}
+        </div>
+
+
+        <div>
+          {dataPorts.map(({ id, title }) => {
+            return (
+              <p key={id} className={`port_text port_location_${id}`}>
+                {title}
+              </p>
+            )
+          })}
+        </div>
 
         <div>
           {dataTransport.map(({ id, title, color, point }) => {
@@ -93,9 +74,9 @@ const MainPage = () => {
                   style={{ background: `${color}` }}
                 >
                   <div>
-                    <div className='cluster_block_text'>
+                    <p className='cluster_block_text'>
                       {title}
-                    </div>
+                    </p>
                     <div className={`transport_marker_line_${id}`} />
                   </div>
                 </div>
@@ -104,6 +85,7 @@ const MainPage = () => {
             )
           })}
         </div>
+
         <div>
           {dataClusters.map(cluster => {
             return (
@@ -114,9 +96,9 @@ const MainPage = () => {
                 className={`cluster_block text_location_${cluster.id}`}
                 style={{ background: `${cluster.color}` }}
               >
-                <div className='cluster_block_text'>
+                <p className='cluster_block_text'>
                   {cluster.title}
-                </div>
+                </p>
               </div>
             )
           })}
@@ -124,22 +106,28 @@ const MainPage = () => {
 
         </div>
         <Background className="background_1" onClick={(e) => changeRoute(e.target.id)} />
-        {/* <SignRusskiy className="sign_russkiy" />
-        <BigStone className="sign_bigstone" />
-        <SignArtem className='sign_artem' />
-        <SignAeroport className='sign_aeroport' />
-        <IconAeroport className='icon_aeroport' />
-        <SignVladivostok className='sign_vladivostok' />
-        <SignTor className='sign_tor' />
-        <SignSanding className='sign_sanding' />
-        <Sign_bridge_elena className='sign_bridge_elena' />
-        <Sign_island_popova className='sign_island_popova' />
-        <Sign_island_reineke className='sign_island_reineke' /> */}
       </div>
     </div>
   )
 }
 export default MainPage;
+
+// import { ReactComponent as SignRusskiy } from '../images/signRusskiy.svg'
+// import { ReactComponent as BigStone } from '../images/signBigStone.svg'
+// import { ReactComponent as SignArtem } from '../images/signArtem.svg'
+// import { ReactComponent as SignAeroport } from '../images/signAeroport.svg'
+// import { ReactComponent as IconAeroport } from '../images/icon_aeroport.svg'
+// import { ReactComponent as SignVladivostok } from '../images/signVladivostok.svg'
+// import { ReactComponent as SignTor } from '../images/signTOR.svg'
+// import { ReactComponent as SignSanding } from '../images/sign_sanding.svg'
+// import { ReactComponent as Sign_bridge_elena } from '../images/sign_bridge_elena.svg'
+// import { ReactComponent as Sign_island_popova } from '../images/sign_island_popova.svg'
+// import { ReactComponent as Sign_island_reineke } from '../images/sign_island_reineke.svg'
+// import { ReactComponent as Sea_ways } from '../images/sea_ways.svg'
+// import { ReactComponent as Land_ways } from '../images/land_ways.svg'
+// import { ReactComponent as Background } from '../images/background_mainpage_finish.svg';
+
+
 
 {/* <div className='container_clusters_list'>
           <div className='descriprion_clusters_list'>КЛАСТЕРЫ</div>
@@ -164,3 +152,14 @@ export default MainPage;
             )
           })}
         </div> */}
+{/* <SignRusskiy className="sign_russkiy" />
+        <BigStone className="sign_bigstone" />
+        <SignArtem className='sign_artem' />
+        <SignAeroport className='sign_aeroport' />
+        <IconAeroport className='icon_aeroport' />
+        <SignVladivostok className='sign_vladivostok' />
+        <SignTor className='sign_tor' />
+        <SignSanding className='sign_sanding' />
+        <Sign_bridge_elena className='sign_bridge_elena' />
+        <Sign_island_popova className='sign_island_popova' />
+        <Sign_island_reineke className='sign_island_reineke' /> */}
